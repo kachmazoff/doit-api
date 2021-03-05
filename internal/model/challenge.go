985d2f1524678ce209476a5e81cf233acb307fc6
kachmazoff <alek.kachmazov@yandex.ru>
2021-03-05 18:43:27 +0400

    Describe Challenge model

---
+++ internal/model/challenge.go
@@ -0,0 +1,14 @@
+package model
+
+import "time"
+
+type Challenge struct {
+	Id               string    `json:"id" db:"id"`
+	Created          time.Time `json:"created" db:"created"`
+	AuthorId         string    `json:"author_id" db:"author_id"`
+	ShowAuthor       bool      `json:"show_author" db:"show_author"`
+	Title            string    `json:"title" db:"title"`
+	Body             string    `json:"body" db:"body"`
+	VisibleType      string    `json:"visible_type" db:"visible_type"`
+	ParticipantsType string    `json:"participants_type" db:"participants_type"`
+}
