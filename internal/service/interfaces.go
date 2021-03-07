6295499ade35ca7012f3069b1553d2a6af1ca3c7
kachmazoff <alek.kachmazov@yandex.ru>
2021-03-07 17:36:48 +0400

    Add Anonymize method to some service interfaces

--- internal/service/interfaces.go
+++ internal/service/interfaces.go
@@ -11,21 +11,26 @@ type Users interface {
 type Challenges interface {
 	Create(challenge model.Challenge) (string, error)
 	GetAll() ([]model.Challenge, error)
+	Anonymize(*model.Challenge) bool
 }
 
 type Timeline interface {
 	GetAll() ([]model.TimelineItem, error)
 	GetCommon() ([]model.TimelineItem, error)
+	Anonymize(*model.TimelineItem) bool
 }
 
 type Participants interface {
 	GetById(id string) (model.Participant, error)
+	Anonymize(*model.Participant) bool
 }
 
 type Notes interface {
 	GetById(id string) (model.Note, error)
+	Anonymize(*model.Note) bool
 }
 
 type Suggestions interface {
 	GetById(id string) (model.Suggestion, error)
+	Anonymize(*model.Suggestion) bool
 }
