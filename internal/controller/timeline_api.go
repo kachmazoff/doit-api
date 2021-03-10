4e4d36101fdab21efea82de18597cc36b277987f
kachmazoff <alek.kachmazov@yandex.ru>
2021-03-10 17:55:49 +0400

    Update timeline api

--- internal/controller/timeline_api.go
+++ internal/controller/timeline_api.go
@@ -4,18 +4,37 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/kachmazoff/doit-api/internal/model"
 )
 
 func (h *Controller) initTimelineRoutes(api *gin.RouterGroup) {
 	timeline := api.Group("/timeline")
 	{
 		timeline.GET("/", h.getAll)
+		timeline.GET("/own", h.userIdentity, h.getOwnTimeline)
+		timeline.GET("/personalized", h.userIdentity, h.getPersonalizedTimeline)
 	}
 }
 
 func (h *Controller) getAll(c *gin.Context) {
 	timeline, err := h.services.Timeline.GetCommon()
 
+	timelineResponse(c, timeline, err)
+}
+
+func (h *Controller) getPersonalizedTimeline(c *gin.Context) {
+	currentUser, _ := getUserId(c)
+	timeline, err := h.services.Timeline.GetForUser(currentUser)
+	timelineResponse(c, timeline, err)
+}
+
+func (h *Controller) getOwnTimeline(c *gin.Context) {
+	currentUser, _ := getUserId(c)
+	timeline, err := h.services.Timeline.GetUserOwn(currentUser)
+	timelineResponse(c, timeline, err)
+}
+
+func timelineResponse(c *gin.Context, timeline []model.TimelineItem, err error) {
 	if err != nil {
 		c.AbortWithStatusJSON(http.StatusInternalServerError, createMessage(err.Error()))
 		return
