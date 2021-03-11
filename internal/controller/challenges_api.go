4e59327d3b20e9023766f50fddeaec3cca892fa7
kachmazoff <alek.kachmazov@yandex.ru>
2021-03-11 23:37:35 +0400

    Add Swagger documentation for createParticipant

--- internal/controller/challenges_api.go
+++ internal/controller/challenges_api.go
@@ -13,6 +13,7 @@ func (h *Controller) initChallengesRoutes(api *gin.RouterGroup) {
 		courses.GET("/", h.getAllChallenges)
 		courses.POST("/", h.userIdentity, h.createChallenge)
 		courses.GET("/:challengeId/participants", h.getParticipantsByChallenge)
+		courses.POST("/:challengeId/participants", h.userIdentity, h.createParticipant)
 	}
 }
 
@@ -67,4 +68,33 @@ func (h *Controller) getParticipantsByChallenge(c *gin.Context) {
 	onlyActive := status == "active"
 	participants, err := h.services.Participants.GetParticipantsInChallenge(challengeId, true, onlyActive)
 	commonJSONResponse(c, participants, err)
+}
+
+// @Summary Create participant
+// @Security Auth
+// @Tags challenges, participants
+// @Description Создание нового участника (регистрация в челлендже в качестве участника)
+// @Accept json
+// @Produce json
+// @Param challengeId path string true "Id челленджа"
+// @Param input body model.Participant true "Модель участника"
+// @Success 200 {object} dto.IdResponse
+// @Failure 400,403 {object} dto.MessageResponse
+// @Router /challenges/{challengeId}/participants [post]
+func (h *Controller) createParticipant(c *gin.Context) {
+	challengeId := c.Param("challengeId")
+
+	var input model.Participant
+	if err := c.BindJSON(&input); err != nil {
+		c.AbortWithStatusJSON(http.StatusBadRequest, err)
+		return
+	}
+
+	currentUser, _ := getUserId(c)
+	input.UserId = currentUser
+	input.ChallengeId = challengeId
+
+	// TODO: test
+	id, err := h.services.Participants.Create(input)
+	handleCreation(c, id, err)
 }
